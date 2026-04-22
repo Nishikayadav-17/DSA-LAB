eed84bcecee7a72f03486fd81507caac5aed2c43
Nishikayadav-17 <nishika.24852@stu.upes.ac.in>
2026-04-22 20:03:20 +0530

    up

--- 100 DAY/heat.py
+++ 100 DAY/heat.py
@@ -1,6 +1,10 @@
-import os
+import subprocess
 import random
 import datetime
+
+def run(cmd):
+    print("👉", cmd)
+    subprocess.run(cmd, shell=True)
 
 start = datetime.date(2026, 3, 1)
 end = datetime.date(2026, 3, 30)
@@ -8,20 +12,20 @@ end = datetime.date(2026, 3, 30)
 date = start
 
 while date <= end:
-    
-    # 70% chance commit ka
+
+    # 70% chance commit
     if random.random() < 0.7:
-        
+
         time = f"{random.randint(10,22)}:{random.randint(0,59)}:00"
         full_date = f"{date}T{time}"
 
-        # file change (IMPORTANT)
+        # file update (IMPORTANT)
         with open("activity.txt", "a") as f:
             f.write(f"{full_date}\n")
 
-        os.system("git add .")
-        os.system(f'git commit --date="{full_date}" -m "commit {full_date}"')
-        os.system("git push")
+        run("git add .")
+        run(f'git commit --date="{full_date}" -m "commit {full_date}"')
+        run("git push")
 
     date += datetime.timedelta(days=1)
 
